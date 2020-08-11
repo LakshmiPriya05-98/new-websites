bd4068904f43eb3e0f59fa552b410037f8af9ffa
lavanyapriya <69449035+lavpri@users.noreply.github.com>
2020-08-11 12:08:55 +0530

    page1

---
+++ Page1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Page1 extends React.Component{
+render()
+{
+return(
+<div>
+  <h1 align='center'><b>ABOUT MY DEVIL SISY</b></h1>
+  <h1 align='center'><u>she loves to eat</u></h1>
+  <h1 align='center'><u>she loves to dance</u></h1>
+  <h1 align='center'><u>she loves to play</u></h1>
+  <h1 align='center'><u>she loves to go beaches</u></h1>
+  <h1 align='center'><b>lava{this.props.name}</b></h1>
+  
+</div>
+)
+}
+}
+export default Page1; 
