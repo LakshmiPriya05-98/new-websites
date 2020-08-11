519bb55679be6505c4f431d5e0774fce54749312
lavanyapriya <69449035+lavpri@users.noreply.github.com>
2020-08-11 12:06:00 +0530

    cake.js

---
+++ Cake.js
@@ -0,0 +1,96 @@
+import React from 'react'
+function Cake(){
+    return(
+    <div>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+<div class='navbar'>
+<div class='subnav'>
+ <button class="subnavbtn">Home<i class="fa fa-caret-down"></i></button>
+ <div class="subnav-content">
+      <a href="#home">Home</a>
+     <b> <h1>SWEET CAKE SHOP</h1></b>
+     </div>
+     </div>
+      <div class='subnav'>
+      <button class="subnavbtn">My menu<i class="fa fa-caret-down"></i></button>
+      <div class="subnav-content">
+      <ul>
+      <h4><b><li><a href="https://cdn.igp.com/f_auto,q_auto,t_prodm/products/p-pineapple-cake-with-chocolate-art-2-kg--6121-m.jpg">pineapple cake=RS:230</a></li></b></h4>
+ <h4><b><li><a href="https://livforcake.com/wp-content/uploads/2017/07/black-forest-cake-thumb.jpg">blackforest cake=RS:500</a></li></b></h4>
+ <h4><b><li><a href="https://www.cakegift.in/sites/default/files/fresh%20-%20fruit%20-%20cake.jpg">fresh fruit cake=RS:390</a></li></b></h4>
+ <h4><b><li><a href="https://d2m1a7vdhhb67a.cloudfront.net/sites/default/files/styles/product_image/public/chocolate-nut-cake-in-delhi-cake0751flav-a.jpg?itok=ZEvXjJGQ">choco nut cake=RS:450</a></li></b></h4>
+ <h4><b><li><a href="https://birthdaycake24.com/uploads/worigin/2017/02/16/demo58a58a0cb2129_07535d7f9aa5afec4f8fe84c209cf045.jpg">photo cake=RS:700</a></li></b></h4>
+ <h4><b><li><a href="https://www.yourcupofcake.com/wp-content/uploads/2014/05/Oreo-Cake-500x500.jpg">oreo cake=RS:900</a></li></b></h4>
+ <h4><b><li><a href="https://trivandrumcakehouse.com/wp-content/uploads/2018/11/Kitkat-Cake-500x500.jpg">kitkat cake=RS:800</a></li></b></h4>
+ <h4><b><li><a href="https://www.cakehut.in/image/cache/data/Kiwi%20Fruit-500x500.jpg">kiwi cake=RS:500</a></li></b></h4>
+ <h4><b><li><a href="https://www.indianhealthyrecipes.com/wp-content/uploads/2017/07/butter-cake-recipe-500x447.jpg">butter cake=RS:200</a></li></b></h4>
+ <h4><b><li><a href="https://www.indianhealthyrecipes.com/wp-content/uploads/2018/07/eggless-vanilla-cake-recipe.jpg">eggless cake=RS:700</a></li></b></h4>
+ <h4><b><li><a href="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/strawberry-cake-jpg-1522267153.jpg">strawberry cake=RS:1500</a></li></b></h4>
+ <h4><b><li><a href="https://i1.wp.com/www.artificiallyreal.com.au/wp-content/uploads/2019/04/WHITE-FOREST-CAKE-2.jpg?fit=533%2C468&ssl=1">white forest cake=RS:1000</a></li></b></h4>
+</ul>
+      </div>
+      </div>
+      <div class='subnav'>
+ <button class="subnavbtn">About<i class="fa fa-caret-down"></i></button>
+ <div class="subnav-content">
+<h2><b><i><p>  Help celebrate a special day or commemorate a significant occasion with a sweet treat from The Cake Shop Bakery.</p></i></b></h2>
+      </div>
+      </div>
+      <div class='subnav'>
+      <button class="subnavbtn">Contact<i class="fa fa-caret-down"></i></button>
+       <div class="subnav-content">
+    <ul>
+      <h4><b><li><a href="#Email Id:cakeshop143@gmail.com">Email Id:cakeshop143@gmail.com</a></li></b></h4>
+     <h4><b><li ><a href="#Phone no:9807612345">Phone no:9807612345</a></li></b></h4>
+     </ul>
+      </div>
+      </div>
+      <div class='subnav'>
+     <button class="subnavbtn">SignUp<i class="fa fa-caret-down"></i></button>
+      <div class="subnav-content">
+      <form>
+          USERNAME/Email Id
+          <h4><b><input type="USERNAME/Email Id" name="USERNAME/Email Id"></input></b></h4>
+          
+          PASSWORD
+          <h4><b><input type="PASSWORD" name="PASSWORD"></input></b></h4>
+          
+          CONFIRM PASSWORD
+          <h4><b><input type="CONFIRM PASSWORD" name="CONFIRM PASSWORD"></input></b></h4>
+          
+          <button type="submit">Signin</button>
+          </form>
+          </div>
+</div>
+<div class='subnav'>
+<button class="subnavbtn">About Delivery<i class="fa fa-caret-down"></i></button>
+<div class="subnav-content">
+<ul>
+   <h4><b> <li><a href="#cash on delivery">cash on delivery</a></li></b></h4>
+    <form>
+    <h4><b> <li><a href="#credit/debit card mode delivery">credit/debit card mode delivery</a></li></b></h4>
+    card holder name
+<input type="card holder name" name="card holder name"></input>
+<br></br>
+card number
+<input type="card number" name="card number"></input>
+<br></br>
+mm/yy
+<input type="mm/yy" name="mm/yy"></input>
+<br></br>
+ccv no
+<input type='ccv no' name='ccv no'></input>
+<br></br>
+<button type="submit">login</button>
+<br></br>
+</form>
+    <br></br>
+    </ul>
+    </div>
+    </div>
+</div>
+    </div>
+)
+}
+export default Cake;
