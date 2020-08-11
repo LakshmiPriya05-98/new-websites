34f1183c0c46c831b3af568a550b646eab9b951c
lavanyapriya <69449035+lavpri@users.noreply.github.com>
2020-08-11 12:08:12 +0530

    cake1.js

---
+++ Cake1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Cake1(){
+    return(
+        <div>
+        <div class="row"></div>
+  <div class="column"></div>
+    <img src="https://cdn.pixabay.com/photo/2017/12/13/11/59/cake-3016621_1280.jpg"></img>
+    <img src="https://cdn.pixabay.com/photo/2014/05/23/23/17/dessert-352475_1280.jpg"></img>
+    <img src="https://cdn.pixabay.com/photo/2016/10/31/18/14/ice-1786311_1280.jpg"></img>
+    <img src="https://cdn.pixabay.com/photo/2015/12/30/11/53/birthday-1114056_1280.jpg"></img>
+    
+    
+    
+  </div>
+    )
+}
+export default Cake1;
